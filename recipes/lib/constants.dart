f596101e28d7f28b4d214bc6df2b0fa33d22f44e
emmakuen <emmaku@protonmail.com>
2021-12-03 23:42:01 +0900

    style recipe cards

---
+++ recipes/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// ------------- TEXTS -------------------- //
+const String kAppTitle = 'Recipe Calculator';
+
+// ------------- TEXT STYLES ---------------- //
+const TextStyle kLabelTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.w700,
+  fontFamily: 'Palatino',
+);
---
+++ recipes/lib/screens/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/constants.dart';
+import 'package:recipes/recipe.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+        elevation: 2,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Image(image: AssetImage(recipe.imageUrl)),
+              const SizedBox(height: 14.0),
+              Text(recipe.label, style: kLabelTextStyle)
+            ],
+          ),
+        ));
+  }
+}
