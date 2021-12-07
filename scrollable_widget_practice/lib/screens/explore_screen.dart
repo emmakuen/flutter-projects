658e197ebd5de08f2d3301b2c2f5e4a691027fca
emmakuen <emmaku@protonmail.com>
2021-12-07 17:03:54 +0900

    add scroll listener

--- scrollable_widget_practice/lib/screens/explore_screen.dart
+++ scrollable_widget_practice/lib/screens/explore_screen.dart
@@ -3,9 +3,33 @@ import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
-  final mockService = MockFooderlichService();
+class ExploreScreen extends StatefulWidget {
   ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  final controller = ScrollController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    if (controller.position.atEdge) {
+      final isTop = controller.position.pixels == 0;
+      if (isTop) {
+        print('at the top');
+      } else {
+        print('at the bottom');
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +38,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: controller,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
