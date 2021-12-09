821af952665142cb684dcac36e05b686c98caa87
emmakuen <emmaku@protonmail.com>
2021-12-09 17:02:44 +0900

    add grocery-item-screen

--- interactive_widgets_practice/lib/screens/grocery_screen.dart
+++ interactive_widgets_practice/lib/screens/grocery_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fooderlich/models/models.dart';
 import 'empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO 4: Add a scaffold widget
-    return const EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => GroceryItemScreen(
+                  onCreate: (item) {
+                    manager.addItem(item);
+                    Navigator.pop(context);
+                  },
+                  onUpdate: (item) {},
+                ),
+              ));
+        },
+      ),
+      body: buildGroceryScreen(),
+    );
   }
-  // TODO: add buildGroceryScreen
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(builder: (context, manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        // TODO 25: Add GroceryListScreen
+        return Container();
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    });
+  }
 }
